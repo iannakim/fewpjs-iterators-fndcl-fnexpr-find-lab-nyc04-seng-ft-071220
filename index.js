65f1e8d2368db0713107f71e49904ce628c60fe0
iannakim <dear.hyunji@gmail.com>
2020-10-02 19:05:01 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+let superbowlWin = (array) => {
+
+let obj  = array.find( e => {
+    return e.result === "W"})
+  if (obj){
+    return obj.year
+  }
+  else {
+    return undefined
+  }
+
+}
